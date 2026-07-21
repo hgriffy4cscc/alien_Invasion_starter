3074d0d876822e607bce3a0d43e23bd039077125
Henry <hgriffy@student.cscc.edu>
2026-07-21 19:44:02 -0400

    Part 3: Created button class + settings for button msg

--- settings.py
+++ settings.py
@@ -34,3 +34,11 @@ class Settings:
         self.alien_fleet_direction = 1
         self.alien_fleet_drop_speed = self.alien_h
 
+        self.button_w = 200
+        self.button_h = 50
+        self.button_color = (0, 135, 50)
+
+        self.text_color = (255, 255, 255)
+        self.button_font_size = 48
+        self.HUD_font_size = 20
+        self.font_file = Path.cwd() / 'Assets' / 'Fonts' / 'Silkscreen' / 'Silkscreen-Bold.ttf'
